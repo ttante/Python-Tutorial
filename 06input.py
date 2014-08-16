97695629591cbc003b4f1513df5c61ef87d2d395
ttante <tylerdinner@gmail.com>
2014-08-15 20:52:40 -0400

    more stuff

--- 06input.py
+++ 06input.py
@@ -1,2 +1,12 @@
 # input in python 2 is called raw_input
 # its just called input in python 3
+print 'type something and it will be repeated!'
+userInput = raw_input()
+print userInput
+if type(userInput) == str:
+	print 'type is a string!'
+else:
+	print 'type is not str'
+
+# print new line
+print ('/n')
