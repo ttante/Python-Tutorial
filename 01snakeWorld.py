dde681e7a31ce7997d2d06381782158cb013cc80
ttante <tylerdinner@gmail.com>
2014-09-24 16:11:05 -0400

    25 done!

--- 01snakeWorld.py
+++ 01snakeWorld.py
@@ -2,8 +2,67 @@
 # The following is for python 2 and below. 
 # In python 3 we would say: print ('our statement') 
 # Other than that there isn't much to worry about btwn versions 2 & 3 for now!
+
+# GETTNG STARTED WITH PYTHON
+
+# The print method lets you print something in Python for others to see
+# To do this simply type print followed by the string of characthers you
+# want to print surrounded by quotes - YOU CAN USE DOULBE OR SINGLE QOUTES
+
+# To combine multiple strings or variables, add a space and plus (+) sign. 
+
 print 'Hello World!' + ' ' + 'There\'s a snake in my boot!'
+# OUTPUT >>> Hello World! There's a snake in my boot!
+
+# The word 'There's' is written with a \, like 'There\'s' - this tells
+# the program to print the single quote instead of treating it as if the 
+# string was ended - this is called 'escaping' a character. 
+
+# A function is something that takes a value and uses that to return another 
+# value. A method is a built in function, in some cases it can be built in by 
+# the person who wrote the program. A function is written with an empty set
+# of parenthesis, as in the type() function. These parenthesis are where you
+# place the data that for method to use - that item or items are called 
+# ARGUMENTS OR PARAMETERS. 
+
+# The Type Function
+
+# The type() method comes built in to the python language. It takes
+# one argument and returns to us the type of data that the parameter is. 
+# What's a data type?
+
+# Remember the print method? When we used it we had to put 'hello world' 
+# bewteen quotes - that's because we printed a 'string' of characters. 
+# In programming, a string like 'donkey kong 64' is different than a number
+# like 23, or, 64. 
+
+# Let's use the type() method to demonstrate
 print type('wazzzzup')
+# >>> String
+
+print type(64)
+# >>> Number
+
+print type('sixty four')
+# >>> String
+
+print type('64')
+# >>> String
+
+
+# SETTING VARIABLES
+
+# A variable is an object you name that you can use to store data in. 
+# You're probably used to them from algebra. You can use them to store
+# just about anything in programming- from equations to functions to other
+# variables and more.
+
+# To set a variable just use a singe equals sign between the variable and what 
+# you want to set it to. 
+
+# Variables set the object on the left side of the equals sign to the oject on the 
+# right side. 
+
 dog = type(23)
 print dog
 
--- 25tuples.py
+++ 25tuples.py
@@ -16,3 +16,151 @@ anotherTuple = ('a', 'b', 'c', 'd')
 print anotherTuple[0]
 # >>>>  'a'
 
+#####################################################
+
+# Sometimes you will want to swap the values of 2 variables, 
+# as in make a equal to b and b equal to a
+
+# The original way to do that would be
+
+a = 1
+b = 2
+temporary = a
+a = b
+b = temp
+
+# >>>> a = 2, b = 1
+
+# THIS IS NOT THE BEST WAY TO DO THIS:
+# Instead use tuple assignment, like THIS
+
+a, b = b, a 
+
+# This makes sets the first element on the left side of the 
+# equals sign to the first element on the right, as well as the
+# second, and so on if there were more. 
+
+#########################################################
+
+# Tuples can be returned from a function, just like strings, 
+# this gives that function the capability of returning multiple 
+# values 
+
+# Example
+# The divmod() method, short for division modulo, divides one number
+# by another and then returns a tuple of the quotient and the remainder
+
+divmodExample = divmod(10, 3)
+print divmodExample
+
+# >>> (3, 1)
+
+
+
+#######################################################
+
+# You can make a function take a tuple as an argument. 
+# Some built in methods only take a certain number of argumernts, 
+# but some, like max(), which finds the max value in any given
+# set, can take any number of arguments. 
+
+# Example
+var myNewTuple = tuple(2, 3, 6, 3 ,2, 3, 6)
+max(*myNewTuple)
+# >>> 6
+
+# THE MAGIC LIES IN THE * IN FRONT OF myNewTuple - this 
+# tells the function it is going to get a tuple of an unknown
+# length. 
+
+
+
+########################################################
+
+# The Zip method
+
+# The zip() method takes two sets of characters, like a list and 
+# a string, and returns a list of tuples, where each tuple
+# has an element from each set of characters. 
+
+# Example
+
+myList = [1, 2, 3]
+myString = 'abc'
+myZipped = zip(myList, myString)
+# >>>  [(1, 'a'), (2, 'b'), (3, 'c')]
+
+# IF ONE OF THE CHACTER SETS IS SHORTER THAN THE OTHER, zip() 
+# WILL STOP AFTER THE LAST ITEM IN THE SHORTER SET
+
+# We can now do this
+for number, letter in myZipped
+	print number, letter
+
+
+# >>>  1 a 
+# >>>  2 b
+# >>>  3 c
+
+
+#########################################################
+
+
+# To traverse a set of characters with their index, use the enumerate() method
+
+# Example
+
+for index, element in enumerate('dog')
+	print index, element
+
+# >>> 0 d
+# >>> 1 o
+# >>> 2 g
+
+
+###########################################################
+
+# You can use the items() method to list all the items in a set of 
+# tuples or items, but THE ORDER OF THE ITEMS WILL NOT COME IN ORDER
+# The items will show up in a random order - it's just the way it goes.
+
+###########################################################
+
+
+# You can use a list of tuples to construct a new dictionary
+
+tupleList = [('d', 0), ('o', 1), ('g', 2)]
+newDictionary = dict(tupleList)
+print newDictionary
+
+# >>> {'d': 0, 'o': 1, 'g': 2}
+
+
+# Now we can traverse items in a dictionary like this: 
+
+for key, value in newDictionary.items():
+	pring value, key
+# >>> 0 d
+# >>> 1 o
+# >>> 2 g
+
+
+##################################################
+
+# The relational operators, like <, >, <=, and >= work with tuples
+# and other sequences. They start with the first character, and if the
+# first 2 elements are equal, it moves to the next until it finds two
+# that are different
+
+# Example
+
+xx = (1, 2, 3)
+yy = (1, 1, 3)
+xx > yy
+# >>> True 
+
+
+aa = (1, 2, 3)
+bb = (1, 2, 4)
+aa >= bb 
+# >>> False
